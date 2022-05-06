f07c532ea2ff3e18bb2d216cb0475049f79df8c4
baihuijun <dreamerbai@163.com>
2022-05-06 09:18:58 +0800

    摸板初建

--- src/views/welcome.vue
+++ src/views/welcome.vue
@@ -1,26 +1,9 @@
 <template>
-  <div class="welcome">
-    <forms></forms>
-    <div class="box"></div>
-  </div>
+  <div class="welcome bg-red-400 hover:bg-blue-600">欢迎界面</div>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
-import forms from '@/components/forms.vue'
-
-// toggleTheme({
-//   scopeName: 'theme-mauve'
-//   // scopeName: 'theme-default'
-//   // 可选，link的href处理，看情况用， 当启用 themePreprocessorPlugin 的 extract后才需要
-//   // customLinkHref: (href) => href,
-//   // 可选，默认对应 themePreprocessorPlugin 的 themeLinkTagId
-//   // themeLinkTagId: "theme-link-tag",
-//   // 可选 "head" || "body"
-//   // themeLinkTagInjectTo: "head",
-//   // 可选，对应 themePreprocessorPlugin 的 multipleScopeVars
-//   // multipleScopeVars
-// })
 </script>
 <style lang="scss" scoped>
 .welcome {
