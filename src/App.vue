3f3f2c5bf36d6cb239372c6b7d442938ac2c2d33
baihuijun <dreamerbai@163.com>
2022-05-10 22:37:01 +0800

    主题色bug

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { GlobalStore } from "@/store"
+import { toggleTheme } from "@zougt/vite-plugin-theme-preprocessor/dist/browser-utils"
+const globalStore = GlobalStore()
+const theme = computed((): string => globalStore.theme)
+watch(
+  () => theme.value,
+  theme => {
+    const htmlDom = document.getElementsByTagName("html")
+    toggleTheme({
+      scopeName: theme
+    })
+    if (theme === "theme-dark") {
+      htmlDom[0].classList.add("dark")
+    } else {
+      htmlDom[0].classList.remove("dark")
+    }
+  },
+  { immediate: true }
+)
+</script>
 
 <template>
   <router-view></router-view>
--- src/layout/Header/components/Theme.vue
+++ src/layout/Header/components/Theme.vue
@@ -16,30 +16,10 @@
 
 <script setup lang="ts">
 import { GlobalStore } from "@/store"
-import { toggleTheme } from "@zougt/vite-plugin-theme-preprocessor/dist/browser-utils"
-
 const globalStore = GlobalStore()
-
-const theme = computed((): string => globalStore.theme)
 const handleSetTheme = (scopeName: string) => {
   globalStore.setTheme(scopeName)
 }
-
-watch(
-  () => theme.value,
-  theme => {
-    const htmlDom = document.getElementsByTagName("html")
-    toggleTheme({
-      scopeName: theme
-    })
-    if (theme === "theme-dark") {
-      htmlDom[0].classList.add("dark")
-    } else {
-      htmlDom[0].classList.remove("dark")
-    }
-  },
-  { immediate: true }
-)
 </script>
 
 <style scoped lang="scss">
